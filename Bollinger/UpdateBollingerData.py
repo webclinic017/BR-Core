a436713a617125d81777b5a0353a0969ca520b62
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-02 16:08:22 +0900

    Add json module in bollinger

--- Bollinger/UpdateBollingerData.py
+++ Bollinger/UpdateBollingerData.py
@@ -6,6 +6,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from GetOptimalPortfolio import GetPriceModule
 from GetStockAndUpateDB import UpdateDBModule
+import json
 
 class GetBollingerDataModule:
     def __init__(self):
